27961e864c02333aebb530b2cdd2f970cc6a31e7
Chlápek Tobias <chlapekt@gytool.cz>
2026-09-10 10:40:54 +0200

    prvni projekt na c# s nejakym basic obsahem ctverce a kruhu

--- PROGRAMS/AreaVolume/Program.cs
+++ PROGRAMS/AreaVolume/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AreaVolume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vyber geometricky tvar");
+            Console.WriteLine("1, ctverec");
+            Console.WriteLine("2, kruh");
+
+            var shape = Console.ReadLine();
+
+            switch (shape)
+            {
+                case "1":
+                    CalculateSquare();
+                    break;
+                case "2":
+                    CalculateCircle();
+                    break;
+                default:
+                    Console.WriteLine("neplatna hodnota!!!");
+                    break;
+            }
+        }
+
+        static void CalculateSquare()
+        {
+            Console.WriteLine("zadej a");
+            string input = Console.ReadLine();
+
+            var a = double.Parse(input);
+
+            var area = a * a;
+            var perimetr = 4 * a; 
+
+            Console.WriteLine($"Obsah: {area}");
+            Console.WriteLine($"Obvod: {perimetr}");
+        }
+
+        static void CalculateCircle()
+        {
+            Console.WriteLine("zadej polomer r");
+            string input = Console.ReadLine();
+
+            var r = double.Parse(input);
+
+            var area = Math.PI * r * r;
+            var perimetr = 2 * Math.PI * r;
+
+            Console.WriteLine($"Obsah: {area}");
+            Console.WriteLine($"Obvod: {perimetr}");
+        }
+    }
+}
